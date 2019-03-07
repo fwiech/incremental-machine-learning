fix gradient variable & ewc calculation

src/main.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     learning_rate_b = 0.0001
     training_iters_b = 30
     batch_size_b = 128
-    lambda_val = 1.# (1./learning_rate_b)
+    lambda_val = (1./learning_rate_b)
 
 
     # get MNIST data
@@ -105,6 +105,8 @@ if __name__ == '__main__':
         learning_rate=0)
     fisher_matrix_gradient_tensors = fisher_matrix_optimizer.compute_gradients(
         nn.loss)
+    
+    # init gradients & variables
     gradients = {}
     variables = {}
     for key, weight in nn.weights.items():
@@ -135,8 +137,8 @@ if __name__ == '__main__':
         logging.info("* TASK A *")
         logging.info("**********")
 
-        compute_task_a(sess, nn, update_a, iter_train_a, iter_test_a,
-                       training_iters_a, fisher_matrix_gradient_tensors)
+        gradients, variables = compute_task_a(sess, nn, update_a, iter_train_a, iter_test_a,
+                       training_iters_a, fisher_matrix_gradient_tensors, gradients, variables)
         
         """ SAVING """
         save_path = saver.save(sess, task_a_model_name)
@@ -150,7 +152,7 @@ if __name__ == '__main__':
     optimizer_b = tf.train.GradientDescentOptimizer(
         learning_rate=learning_rate_b)
     ewc, ewc_print = nn.compute_ewc(
-        gradients, variables, lambda_val=lambda_val)
+        gradients, variables, lam=lambda_val)
     ewc_print = tf.print("****************new iteration ****************", ewc_print, output_stream="file://tmp/tensor_" + str(ts) + ".log")
     update_b = optimizer_b.minimize(
         tf.add(nn.loss, ewc))

src/model/network.py
@@ -60,7 +60,7 @@ class Network():
             tf.argmax(self.logits, 1), tf.argmax(y, 1))
         self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-    def compute_ewc(self, gradients, variables, lambda_val):
+    def compute_ewc(self, gradients, variables, lam):
         
         logging.debug("************")
         logging.debug("* CALC EWC *")
@@ -78,9 +78,8 @@ class Network():
             
             # calc EWC appendix
             subAB = tf.subtract(varsB, variables[key])
-            powAB = tf.pow(subAB, 2)
+            powAB = tf.square(subAB)
             multiplyF = tf.multiply(powAB, gradient)
-            lambda_multiply = tf.multiply(multiplyF, lambda_val)
 
             prints.append("------------------")
             prints.append(key)
@@ -98,10 +97,9 @@ class Network():
             prints.append("----")
 
             if ewc is 0:
-                ewc = tf.reduce_sum(lambda_multiply)
-                pprint(tf.reduce_sum(lambda_multiply))
+                ewc = (lam/2) * tf.reduce_sum(multiplyF)
             else:
-                ewc += tf.reduce_sum(lambda_multiply)
+                ewc += (lam/2) * tf.reduce_sum(multiplyF)
             
             prints.append("CURRENT EWC: ")
             prints.append(ewc)

src/task_a.py
@@ -14,7 +14,7 @@ import logging
 from pprint import pprint
 
 
-def compute_task_a(sess, nn, update, iter_train_a, iter_test_a, training_iters_a, fisher_matrix_gradient_tensors):
+def compute_task_a(sess, nn, update, iter_train_a, iter_test_a, training_iters_a, fisher_matrix_gradient_tensors, gradients, variables):
     """ TRAINING """
     logging.info("************")
     logging.info("* TRAINING *")
@@ -55,8 +55,8 @@ def compute_task_a(sess, nn, update, iter_train_a, iter_test_a, training_iters_a
     # init iterator
     sess.run(iter_test_a)
 
-    gradients = {}
-    variables = {}
+    gradients_np = {}
+    variables_np = {}
 
     iterations = 0
     try:
@@ -71,23 +71,24 @@ def compute_task_a(sess, nn, update, iter_train_a, iter_test_a, training_iters_a
 
                 gradient_power = np.power(gradient, 2)
 
-                if gard_name in gradients:
-                    gradients[gard_name] = np.add(
-                        gradients[gard_name], gradient_power)
+                if gard_name in gradients_np:
+                    gradients_np[gard_name] = np.add(
+                        gradients_np[gard_name], gradient_power)
                 else:
-                    gradients[gard_name] = gradient_power
+                    gradients_np[gard_name] = gradient_power
 
-                if gard_name not in variables:
-                    variables[gard_name] = result[1]
+                if gard_name not in variables_np:
+                    variables_np[gard_name] = result[1]
 
     except tf.errors.OutOfRangeError:
 
         logging.debug("*************")
-        logging.debug("* GRADIENTS *")
-        for key, gradient in gradients.items():
+        logging.debug("* gradients_np *")
+        for key, gradient in gradients_np.items():
             gradient = np.true_divide(gradient, iterations)
-            gradients[key] = tf.Variable(gradient, name="gradient_" + key)
+            gradients[key] = gradients[key].assign(gradient.astype(np.float32))
             logging.debug("key: " + key)
+            logging.debug("gradient: " + str(gradient))
             logging.debug("shape: " + str(gradient.shape))
             logging.debug("min: " + str(gradient.min()))
             logging.debug("max: " + str(gradient.max()))
@@ -95,10 +96,13 @@ def compute_task_a(sess, nn, update, iter_train_a, iter_test_a, training_iters_a
 
         logging.debug("*************")
         logging.debug("* VARIABLES *")
-        for key, variable in variables.items():
-            variables[key] = tf.Variable(variable, name="variable_" + key)
+        for key, variable in variables_np.items():
+            variables[key] = variables[key].assign(variable.astype(np.float32))
             logging.debug("key: " + key)
+            logging.debug("variable: " + str(variable))
             logging.debug("shape: " + str(variable.shape))
             logging.debug("min: " + str(variable.min()))
             logging.debug("max: " + str(variable.max()))
             logging.debug("---")
+
+    return gradients, variables