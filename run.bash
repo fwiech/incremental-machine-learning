bd591b31fee3e14f474372044fe6d7556509adff
Alexander Gepperth <alexander.gepperth@cs.hs-fulda.de>
2019-03-20 15:21:56 +0100

    ./.

--- run.bash
+++ run.bash
@@ -6,7 +6,4 @@
 python3 src/main.py --classes 0 1 2 3 4 5 6 7 8  --learnrate 0.001 --iterations 2500 --batch 100 -s FISH1 --batch_fisher 1000 -d 100
 python3 src/main.py --classes 9 --learnrate 0.00001 --lambda 1000000 --iterations 2500 --batch 100  --previous FISH1 --batch_fisher 1 -d 100
 
-### test testing arrangements!!
-### check whether fm is loaded correctly!!
-
 
