1be5e8c321176b5a9d70044a979288aa30ffdcd2
Florian Wiech <git@florianwiech.me>
2019-03-22 11:05:30 +0100

    update run bash

--- run.bash
+++ run.bash
@@ -3,5 +3,5 @@
 #python3 src/main.py --classes 0 1 2 3 4 5 6 7 8 9 --learnrate 0.001 --lambda 1000 --iterations 2500 --batch 100 -s FISH2 --previous FISH1 --batch_fisher 1 -d 100 --permute 0
 
 #setup for 9-1!
-python3 src/main.py --classes 0 1 2 3 4 5 6 7 8  --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1000 -s FISH1
-python3 src/main.py --classes 9 --learnrate 0.00001 --lambda 1000000 --iterations 2500 --batch 100 --previous FISH1
+python3 src/main.py --classes 0 1 2 3 4 5 6 7 8  --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1000 -s FISH
+python3 src/main.py --classes 9 --learnrate 0.00001 --lambda 1000000 --iterations 2500 --batch 100 --previous FISH
