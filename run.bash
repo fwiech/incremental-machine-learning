f6b0836d4e0fd59a9de9410c7e4a8df7e1ce30c1
Florian Wiech <git@florianwiech.me>
2019-04-01 16:58:30 +0200

    update run bash

--- run.bash
+++ run.bash
@@ -1,48 +1,165 @@
 #!/bin/bash
 
 
-# # szenario 9-1
-python3 src/main.py --classes 0 1 2 3 4 5 6 7 8 --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1000 -s 91_A_bs1000_lam10000000 -d 50
-python3 src/main.py --classes 9 --learnrate 0.00001 -lambda 1000000 --iterations 2500 --batch 100 --previous 91_A_bs1000_lam10000000 --batch_fisher 1000 -s 91_B_bs1000_lam10000000 -d 50
-
-# szenario 5-5
-python3 src/main.py --classes 0 1 2 3 4  --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1000 -s 55_A_bs1000_lam10000000 -d 50
-python3 src/main.py --classes 5 6 7 8 9 --learnrate 0.001 -lambda 1000000 --iterations 2500 --batch 100 --previous 55_A_bs1000_lam10000000 --batch_fisher 1000 -s 55_B_bs1000_lam10000000 -d 50
-
-szenrio permuted
-python3 src/main.py --classes 0 1 2 3 4 5 6 7 8 9 --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1000 -s 'PM_A_bs1000_lam10000000' -d 50
-python3 src/main.py --classes 0 1 2 3 4 5 6 7 8 9 --permute 0 --learnrate 0.00001 -lambda 1000000 --iterations 25000 --batch 100 --previous 'PM_A_bs1000_lam10000000' --batch_fisher 10000 -s 'PM_B_bs1000_lam10000000' -d 50
+echo "Task T2 on BS1000"
+python3 src/main.py \
+--classes 5 6 7 8 9 \
+--learnrate 0.00001 \
+--iterations 2500 \
+--batch 100 \
+--previous '55_A_BS1000' \
+--batch_fisher 1000 \
+-d 1000
 
 
+# echo "P10-10"
+
+# echo "train first network with BS1000"
+# python3 src/main.py \
+# --classes 0 1 2 3 4 5 6 7 8 9 \
+# --learnrate 0.001 \
+# --iterations 2500 \
+# --batch 100 \
+# --batch_fisher 1000 \
+# -s 'PM_A_BS1000' \
+# -d 50
+
+# echo "recalculate the trained network with FIM"
+# python3 src/main.py \
+# --classes 0 1 2 3 4 5 6 7 8 9 \
+# --learnrate 0.001 \
+# --iterations 2500 \
+# --batch 100 \
+# --batch_fisher 1 \
+# --previous 'PM_A_BS1000' \
+# --notrain \
+# -s 'PM_A_FIM' \
+# -d 50
+
+# echo "Task T2 on BS1000"
+# python3 src/main.py \
+# --classes 0 1 2 3 4 5 6 7 8 9 \
+# --learnrate 0.00001 \
+# --iterations 20000 \
+# --batch 100 \
+# --previous 'PM_A_BS1000' \
+# --batch_fisher 1000 \
+# --permute 0 \
+# -s 'PM_B_BS1000' \
+# -d 1000
+
+# echo "Task T2 on FIM"
+# python3 src/main.py \
+# --classes 0 1 2 3 4 5 6 7 8 9 \
+# --learnrate 0.00001 \
+# --iterations 20000 \
+# --batch 100 \
+# --previous 'PM_A_FIM' \
+# --batch_fisher 1000 \
+# --permute 0 \
+# -s 'PM_B_FIM' \
+# -d 1000
 
 
 
-# DEFAULT
 
-# # szenario 9-1
-# python3 src/main.py --classes 0 1 2 3 4 5 6 7 8 --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1000 -s 91_A -d 50
-# python3 src/main.py --classes 9 --learnrate 0.00001 --iterations 2500 --batch 100 --previous 91_A --batch_fisher 1000 -s 91_B -d 50
+# echo "D5-5"
 
-# # szenario 5-5
-# python3 src/main.py --classes 0 1 2 3 4  --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1000 -s 55_A -d 50
-# python3 src/main.py --classes 5 6 7 8 9 --learnrate 0.001 --iterations 2500 --batch 100 --previous 55_A --batch_fisher 1000 -s 55_B -d 50
+# echo "train first network with BS1000"
+# python3 src/main.py \
+# --classes 0 1 2 3 4 \
+# --learnrate 0.001 \
+# --iterations 2500 \
+# --batch 100 \
+# --batch_fisher 1000 \
+# -s '55_A_BS1000' \
+# -d 50
 
-# szenrio permuted
-# python3 src/main.py --classes 0 1 2 3 4 5 6 7 8 9 --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1000 -s 'PM_A_BS1000' -d 50
-# python3 src/main.py --classes 0 1 2 3 4 5 6 7 8 9 --permute 0 --learnrate 0.00001 --iterations 25000 --batch 100 --previous 'PM_A_BS1000' --batch_fisher 1000 -s 'PM_B_BS1000' -d 50
+# echo "recalculate the trained network with FIM"
+# python3 src/main.py \
+# --classes 0 1 2 3 4 \
+# --learnrate 0.001 \
+# --iterations 2500 \
+# --batch 100 \
+# --batch_fisher 1 \
+# --previous '55_A_BS1000' \
+# --notrain \
+# -s '55_A_FIM' \
+# -d 50
+
+# echo "Task T2 on BS1000"
+# python3 src/main.py \
+# --classes 5 6 7 8 9 \
+# --learnrate 0.00001 \
+# --iterations 2500 \
+# --batch 100 \
+# --previous '55_A_BS1000' \
+# --batch_fisher 1000 \
+# -s '55_B_BS1000' \
+# -d 50
+
+# echo "Task T2 on FIM"
+# python3 src/main.py \
+# --classes 5 6 7 8 9 \
+# --learnrate 0.00001 \
+# --iterations 2500 \
+# --batch 100 \
+# --previous '55_A_FIM' \
+# --batch_fisher 1000 \
+# -s '55_B_FIM' \
+# -d 50
 
 
 
-# # EWC ORIGINAL
 
-# # szenario 9-1
-# python3 src/main.py --classes 0 1 2 3 4 5 6 7 8 --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1 -s EWC_91_A -d 50
-# python3 src/main.py --classes 9 --learnrate 0.00001 --iterations 2500 --batch 100 --previous EWC_91_A --batch_fisher 1000 -s EWC_91_B -d 50
 
-# # szenario 5-5
-# python3 src/main.py --classes 0 1 2 3 4  --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1 -s EWC_55_A -d 50
-# python3 src/main.py --classes 5 6 7 8 9 --learnrate 0.00001 --iterations 2500 --batch 100 --previous EWC_55_A --batch_fisher 1 -s EWC_55_B -d 50
+# echo "D9-1"
 
-# szenrio permuted
-# python3 src/main.py --classes 0 1 2 3 4 5 6 7 8 9 --learnrate 0.001 --iterations 2500 --batch 100 --batch_fisher 1 -s 'PM_A_BS1' -d 50
-# python3 src/main.py --classes 0 1 2 3 4 5 6 7 8 9 --permute 0 --learnrate 0.00001 --iterations 25000 --batch 100 --previous 'PM_A_BS1' --batch_fisher 1 -s 'PM_B_BS1' -d 50
+# echo "train first network with BS1000"
+# python3 src/main.py \
+# --classes 0 1 2 3 4 5 6 7 8 \
+# --learnrate 0.001 \
+# --iterations 2500 \
+# --batch 100 \
+# --batch_fisher 1000 \
+# -s '91_A_BS1000' \
+# -d 50
+
+# echo "recalculate the trained network with FIM"
+# python3 src/main.py \
+# --classes 0 1 2 3 4 5 6 7 8 \
+# --learnrate 0.001 \
+# --iterations 2500 \
+# --batch 100 \
+# --batch_fisher 1 \
+# --previous '91_A_BS1000' \
+# --notrain \
+# -s '91_A_FIM' \
+# -d 50
+
+# echo "Task T2 on BS1000"
+# python3 src/main.py \
+# --classes 9 \
+# --learnrate 0.00001 \
+# --iterations 2500 \
+# --batch 100 \
+# --previous '91_A_BS1000' \
+# --batch_fisher 1000 \
+# -s '91_B_BS1000' \
+# -d 50
+
+# echo "Task T2 on FIM"
+# python3 src/main.py \
+# --classes 9 \
+# --learnrate 0.00001 \
+# --iterations 2500 \
+# --batch 100 \
+# --previous '91_A_FIM' \
+# --batch_fisher 1000 \
+# -s '91_B_FIM' \
+# -d 50
+
+
+
+
+
